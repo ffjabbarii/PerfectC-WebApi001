5421215cb6eae9a83ab5de3d6129b25986baa539
Peter Ruß <peterruss1996@gmail.com>
2024-08-18 23:25:58 +0200

    feat: defining the Interface for the ProductService Class

--- ApiTemplate/Services/Interfaces/IProductService.cs
+++ ApiTemplate/Services/Interfaces/IProductService.cs
@@ -13,6 +13,37 @@ namespace ApiTemplate.Services.Interfaces
     /// </summary>
     public interface IProductService
     {
-        
+        /// <summary>
+        /// Retrieves a collection of products asynchronously.
+        /// </summary>
+        /// <returns> A collection of <see cref="Product"/> entities. </returns>
+        Task<IEnumerable<Product>> GetProducts();
+
+        /// <summary>
+        /// Retrieves a product by its unique identifier asynchronously.
+        /// </summary>
+        /// <param name="id"> The unique identifier of the product to retrieve. </param>
+        /// <returns> The <see cref="Product"/> entity with the specified identifier or null if not found. </returns>
+        Task<Product?> GetProductById(int id);
+
+        /// <summary>
+        /// Adds a new product to the collection asynchronously.
+        /// </summary>
+        /// <param name="product"> The product to add to the collection. </param>
+        Task AddProduct(Product product);
+
+        /// <summary>
+        /// Updates an existing product in the collection asynchronously.
+        /// </summary>
+        /// <param name="product"> The product to update in the collection. </param>
+        /// <returns> The updated <see cref="Product"/> entity. </returns>
+        Task<Product> UpdateProduct(Product product);
+
+        /// <summary>
+        /// Deletes a product from the collection asynchronously.
+        /// </summary>
+        /// <param name="id"> The unique identifier of the product to delete. </param>
+        /// <returns> A boolean value indicating whether the product was successfully deleted. </returns>
+        Task<bool> DeleteProduct(int id);
     }
 }
