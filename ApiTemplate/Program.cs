60080b77c35bd62f5257c19d792b5cba9d24be18
Peter Ruß <peterruss1996@gmail.com>
2024-08-18 22:26:46 +0200

    setup: removing examples from project

--- ApiTemplate/Program.cs
+++ ApiTemplate/Program.cs
@@ -7,16 +7,12 @@ namespace ApiTemplate
         {
             var builder = WebApplication.CreateBuilder(args);
 
-            // Add services to the container.
-
             builder.Services.AddControllers();
-            // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
             builder.Services.AddEndpointsApiExplorer();
             builder.Services.AddSwaggerGen();
 
             var app = builder.Build();
 
-            // Configure the HTTP request pipeline.
             if (app.Environment.IsDevelopment())
             {
                 app.UseSwagger();
@@ -26,7 +22,6 @@ namespace ApiTemplate
             app.UseHttpsRedirection();
 
             app.UseAuthorization();
-
 
             app.MapControllers();
 
