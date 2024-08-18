c877ce5b4f0cb34c94fd0e6bcd4fef47a2f8f71e
Peter Ruß <peterruss1996@gmail.com>
2024-08-19 00:58:36 +0200

    tests: added test packages and setup

---
+++ ApiTemplateTests/Controllers/ProductControllerTests.cs
@@ -0,0 +1,27 @@
+﻿#region usings -----------------------------------------------------------------
+
+using ApiTemplate.Services.Interfaces;
+using Microsoft.Extensions.Logging;
+using Moq;
+using ApiTemplate.Controllers.v1;
+using ApiTemplate.Models;
+using Microsoft.AspNetCore.Mvc;
+
+#endregion
+
+namespace ApiTemplateTests.Controllers
+{
+    public class ProductControllerTests
+    {
+        private readonly Mock<IProductService> _mockProductService = new Mock<IProductService>();
+        private readonly Mock<ILogger<ProductsController>> _mockLogger = new Mock<ILogger<ProductsController>>();
+        private readonly ProductsController _controller;
+
+        public ProductControllerTests()
+        {
+            _controller = new ProductsController(_mockProductService.Object, _mockLogger.Object);
+        }
+
+        
+    }
+}
---
+++ ApiTemplateTests/Services/ProductServiceTests.cs
@@ -0,0 +1,24 @@
+﻿#region usings -----------------------------------------------------------------
+
+using ApiTemplate.Services;
+using Microsoft.Extensions.Logging;
+using Moq;
+using ApiTemplate.Models;
+
+#endregion
+
+namespace ApiTemplateTests.Services
+{
+    public class ProductServiceTests
+    {
+        private readonly Mock<ILogger<ProductService>> _mockLogger = new Mock<ILogger<ProductService>>();
+        private readonly ProductService _productService;
+
+        public ProductServiceTests()
+        {
+            _productService = new ProductService(_mockLogger.Object);
+        }
+
+        
+    }
+}
